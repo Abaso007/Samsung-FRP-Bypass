45cf273660fa28880b386993582f0fe58b80ba6a
Sourcery AI <>
2023-08-24 23:01:24 +0000

    'Refactored by Sourcery'

--- usbswitcher.py
+++ usbswitcher.py
@@ -22,25 +22,24 @@ def samsungGalaxyToModemMode() -> bool:
         print("No samsung device detected over USB")
         return False
 
-    if dev is not None:
-        print(f"Samsung device {dev.product} from {dev.manufacturer} detected with {dev.bNumConfigurations} available USB configurations")
-        actualConfig = dev.get_active_configuration().bConfigurationValue
-        print(f"Device is actually in USB configuration {actualConfig}")
-        if actualConfig == USB_MODEM_CONFIGURATION:
-            print(f"Device is already in modem mode (USB configuration {USB_MODEM_CONFIGURATION}), skipping USB switching")
-            return True
-        
-        is_ok = setUSBConfig(dev, USB_MODEM_CONFIGURATION)
-        if not is_ok:
-            # We simply retry...
-            print("Did not work the first time, retrying once")
-            is_ok = setUSBConfig(dev, USB_MODEM_CONFIGURATION)
+    print(f"Samsung device {dev.product} from {dev.manufacturer} detected with {dev.bNumConfigurations} available USB configurations")
+    actualConfig = dev.get_active_configuration().bConfigurationValue
+    print(f"Device is actually in USB configuration {actualConfig}")
+    if actualConfig == USB_MODEM_CONFIGURATION:
+        print(f"Device is already in modem mode (USB configuration {USB_MODEM_CONFIGURATION}), skipping USB switching")
+        return True
 
-        if is_ok:
-            print("USB Configuration sucessfully switched to modem mode")
-        else:
-            print(f"Unable to set USB configuration {USB_MODEM_CONFIGURATION}, it can happen if USB Debugging is already enabled")
-        return is_ok
+    is_ok = setUSBConfig(dev, USB_MODEM_CONFIGURATION)
+    if not is_ok:
+        # We simply retry...
+        print("Did not work the first time, retrying once")
+        is_ok = setUSBConfig(dev, USB_MODEM_CONFIGURATION)
+
+    if is_ok:
+        print("USB Configuration sucessfully switched to modem mode")
+    else:
+        print(f"Unable to set USB configuration {USB_MODEM_CONFIGURATION}, it can happen if USB Debugging is already enabled")
+    return is_ok
         
 if __name__ == "__main__":
     samsungGalaxyToModemMode()
